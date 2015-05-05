c49616cd91b0dda086a4be1f25a2f4107dd62bb9
Erick <sr.tama@outlook.com>
2015-05-04 23:14:15 -0300

    Revert "Segundo"
    This reverts commit ebe92ef3a1b40811ae503ffec9db2fe0f2558d64.

--- questao2.js
+++ questao2.js
@@ -1,9 +1,11 @@
-function questao2(array_,inicial) {  
-  for (var i = 0; i < array_.length; i++) {    
+function questao2(array_,inicial) {
+  
+  for (var i = 0; i < array_.length; i++) {
+    
     if (array_[i].indexOf(inicial) !== 0){
         array_.splice(i,1);
         i=i-1
     }	 
-}	
-    console.log(array_);
+}
+	console.log(array_);
 }
